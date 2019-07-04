ca55ec13daab2a6027ebbfb2845637c82d4494a1
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-07-05 00:39:43 +0900

    feat: set bundlesize

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,7 @@
 import { markdown } from 'danger';
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const report = JSON.parse(fs.readFileSync('./output.json', 'utf-8'));
+const report = JSON.parse(fs.readFileSync('./output.json', 'utf-8'));
 
-markdown('### aaaa');
-// markdown(report.join(''));
+markdown(report.join(''));
